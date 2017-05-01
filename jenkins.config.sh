46233f569bed8fcee48fa5d4d12aec09a3bfc217
FunThomas424242 <funthomas424242@gmail.com>
2017-05-01 17:50:19 +0200

    refactoring

--- jenkins.config.sh
+++ jenkins.config.sh
@@ -6,26 +6,25 @@ echo DIR:  `pwd`
 
 sleep 3m
 
-mkdir /var/jenkins_home/cskripte/
-cp /var/jenkins_home/war/WEB-INF/jenkins-cli.jar /var/jenkins_home/cskripte/jenkins-cli.jar
+# mkdir /var/jenkins_home/cskripte/
+# cp /var/jenkins_home/war/WEB-INF/jenkins-cli.jar /var/jenkins_home/cskripte/jenkins-cli.jar
 
 
-pass=`cat /var/jenkins_home/secrets/initialAdminPassword`
+
 
 echo "INFO: CREATE PIPLINE JOB - BEGIN"
 
-java -jar cskripte/jenkins-cli.jar -s http://localhost:8080/ create-job jobName < skripte/job.xml --username admin --password ${pass}
+java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ create-job pipeline < skripte/job.xml --username admin --password `/bin/cat /var/jenkins_home/secrets/initialAdminPassword`
 
 echo "INFO: CREATE PIPLINE JOB - END"
 
 sleep 3m
 
-java -jar cskripte/jenkins-cli.jar -s http://localhost:8080/ safe-restart --username admin --password ${pass}
+java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-restart --username admin --password `/bin/cat /var/jenkins_home/secrets/initialAdminPassword`
 
 sleep 3m
 
-java -jar cskripte/jenkins-cli.jar -s http://localhost:8080/ safe-shutdown --username admin --password ${pass}
+java -jar /var/jenkins_home/war/WEB-INF/jenkins-cli.jar -s http://localhost:8080/ safe-shutdown --username admin --password `/bin/cat /var/jenkins_home/secrets/initialAdminPassword`
 
 sleep 1m
 
-kill %1
